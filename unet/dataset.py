55d3fd492d7bac72ef62dc81484abe496d61bbf7
IvanNovgorodtsev <ivan.novgorodtsev@gmail.com>
2021-05-07 18:34:37 +0200

    preparing image tiling

--- unet/dataset.py
+++ unet/dataset.py
@@ -5,6 +5,7 @@ from torch.utils.data import DataLoader
 import numpy as np
 import torch
 import time
+import openslide
 
 n_tiles = 16
 tile_size = 256
@@ -13,11 +14,19 @@ def get_tiles(img, mode=0):
     
     result = []
     h, w, c = img.shape
-
+    print(f'img.shape: {img.shape}')
+    # number of "pixels" we need to pad both ways
     pad_h = (tile_size - h % tile_size) % tile_size + ((tile_size * mode) // 2)
     pad_w = (tile_size - w % tile_size) % tile_size + ((tile_size * mode) // 2)
-    img2 = np.pad(img, [[pad_h // 2, pad_h - pad_h // 2], [pad_w // 2, pad_w - pad_w // 2], [0, 0]],constant_values=255)
-    print(h,w,c)
+
+    # padded images, which can be divided each dimension by tile_size
+    img2 = np.pad(img, [[pad_h // 2, pad_h - pad_h // 2], [pad_w // 2, pad_w - pad_w // 2], [0, 0]], constant_values=255)
+    print(f'img2.shape: {img2.shape}')
+
+    # getting the number of tiles in the padded image (getting the shape)
+    img3 = img2.reshape(img2.shape[0] // tile_size, tile_size, img2.shape[1] // tile_size, tile_size, 3)
+    img3.shape
+    print(f'img3.shape: {img3.shape}')
     time.sleep(60)
     return 0
 
@@ -37,8 +46,10 @@ class Dataset(Dataset):
         img_path = os.path.join(self.image_dir, self.images[index])
         mask_path = os.path.join(self.mask_dir, self.masks[index])
 
-        image = io.imread(img_path)
-        image = np.asarray(image, dtype=np.float32)
+        image = openslide.OpenSlide(img_path)
+        image = image.read_region((0,0), 2, image.level_dimensions[2])
+        image = np.asarray(image)[:,:,0:3]
+        print(img_path)
         mask = io.imread(mask_path)
 
         tiles = get_tiles(image)
